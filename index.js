054b43413b9ca7db4dc0f2d9519bc671fb53a43f
AvraamMavridis <avraammauridis@gmail.com>
2020-09-18 06:47:23 +0200

    Chore: Install core depedencies

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('child_process').exec;
+
+function execute(command, callback){
+   exec(command, function(error, stdout, stderr){ callback(stdout); });
+};
 
 // git difftool origin/master... --name-only
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+execute('git difftool origin/master... --name-only', console.log)
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
